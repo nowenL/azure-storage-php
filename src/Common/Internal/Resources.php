35f4290c771e1f5f1d55157682c0171e6c7ab06d
Longwen Lu <longlu@microsoft.com>
2016-03-27 20:38:01 +0800

    Bump up REST API version to 2015-04-05

--- src/Common/Internal/Resources.php
+++ src/Common/Internal/Resources.php
@@ -247,7 +247,7 @@ class Resources
 
     // Header values
     const SDK_USER_AGENT                                = 'Azure-SDK-For-PHP/0.4.1';
-    const STORAGE_API_LATEST_VERSION                    = '2012-02-12';
+    const STORAGE_API_LATEST_VERSION                    = '2015-04-05';
     const SM_API_LATEST_VERSION                         = '2011-10-01';
     const DATA_SERVICE_VERSION_VALUE                    = '1.0;NetFx';
     const MAX_DATA_SERVICE_VERSION_VALUE                = '2.0;NetFx';
